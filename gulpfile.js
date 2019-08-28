3f39c1004aea5f186ea7cdce0ab37c147c2a98c9
Jeffrey Onochie <jeffreyonochie@gmail.com>
2019-08-28 15:18:58 +0100

    create a lockfile as npm-shrinkwrap.json

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,21 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
+    mix.styles([
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        'libs/bootstrap.min.css',
+        'libs/fontawesome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css',
+        'libs/styles.css'
+    ], './public/css/All.css');
+    mix.scripts([
+        'libs/bootstrap.js',
+        'libs/bootstrap.min.js',
+        'libs/jquery.js',
+        'libs/metisMenu.js',
+        'libs/sb-admin-2.js',
+        'libs/scripts.js'
+    ], './public/js/All.js');
 });
